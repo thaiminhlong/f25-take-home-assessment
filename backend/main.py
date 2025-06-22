b5436c3d6686dda14a44208553df3b9f6a077d37
thaiminhlong <thaiminhlongvn@gmail.com>
2025-06-22 01:23:59 -0400

    Code for assessment

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Any, Optional
 import uvicorn
+from dotenv import load_dotenv
+import os
+import httpx
+import uuid
+
+load_dotenv()
 
 app = FastAPI(title="Weather Data System", version="1.0.0")
 
@@ -25,6 +31,12 @@ class WeatherRequest(BaseModel):
 class WeatherResponse(BaseModel):
     id: str
 
+## API Key
+WeatherStackAPI = os.getenv("WeatherStackAPI")
+
+## URL
+WeatherStackURL = r"http://api.weatherstack.com/current"
+
 @app.post("/weather", response_model=WeatherResponse)
 async def create_weather_request(request: WeatherRequest):
     """
@@ -34,7 +46,85 @@ async def create_weather_request(request: WeatherRequest):
     3. Stores combined data with unique ID in memory
     4. Returns the ID to frontend
     """
-    pass
+    params = {
+        "access_key": WeatherStackAPI,
+        "query": request.location
+    }
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(WeatherStackURL, params=params)
+            weather_data = response.json()
+            
+            weather_id = str(uuid.uuid4())
+
+            if "error" in weather_data:
+                raise HTTPException(status_code = 400, detail = "WeatherStack API Error")
+
+            combined_data = {
+                "weather id": weather_id,
+                "date": request.date,
+                "location": request.location,
+                "notes": request.notes,
+                "weather_api_response": weather_data
+            }
+
+            weather_storage[weather_id] = combined_data
+            return WeatherResponse(id = weather_id)
+       
+        except httpx.RequestError as e:
+            raise HTTPException(status_code = 502, detail = f"Network Error: {str(e)}")
+       
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code = e.response.status_code, detail = f"Error from WeatherStackAPI: {str(e)}")
+       
+        except Exception as e:
+            raise HTTPException(status_code = 500, detail = "Internal Server Errors")
+    
+@app.get("/weather/astro/{weather_id}")
+async def get_astronomy_data(weather_id: str):
+    '''
+    Get astronomy data through weather_storage
+    '''
+    if weather_id not in weather_storage:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+    
+    data = weather_storage[weather_id]
+    try:
+        astro = data["weather_api_response"]["current"]["astro"]
+        return astro
+    except Exception:
+        raise HTTPException(status_code=404, detail="No data available")
+
+@app.get("/weather/location/{weather_id}")
+async def get_precise_location(weather_id: str):
+    '''
+    Get the location through weather_storage
+    '''
+    if weather_id not in weather_storage:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+    
+    data = weather_storage[weather_id]
+    try:
+        location = data["weather_api_response"]["location"]
+        return location
+    except Exception:
+        raise HTTPException(status_code=404, detail="No data available")
+
+@app.get("/weather/air-quality/{weather_id}")
+async def get_air_quality_data(weather_id: str):
+    '''
+    Get air quality data through weather_storage
+    '''
+    if weather_id not in weather_storage:
+        raise HTTPException(status_code=404, detail="Weather data not found")
+    
+    data = weather_storage[weather_id]
+    try:
+        air_data = data["weather_api_response"]["current"]["air_quality"]
+        return air_data
+    except Exception:
+        raise HTTPException(status_code=404, detail="No data available")
+
 
 @app.get("/weather/{weather_id}")
 async def get_weather_data(weather_id: str):
@@ -46,6 +136,7 @@ async def get_weather_data(weather_id: str):
         raise HTTPException(status_code=404, detail="Weather data not found")
     
     return weather_storage[weather_id]
+
 
 
 if __name__ == "__main__":
